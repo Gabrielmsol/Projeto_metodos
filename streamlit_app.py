1dffe74b46a90e25b72a88514389c172732c017c
Gabrielmsol <129221473+Gabrielmsol@users.noreply.github.com>
2023-06-21 04:21:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ import numpy as np
 import time
 
 
+# Functions
 def plot0(grid):
 
     plt.style.use('dark_background')
@@ -62,6 +63,7 @@ def ani1(grid):
 
 
 def run_da_game(rows_cols, probability):
+
     grid = create_grid(rows_cols, probability)
 
     plt.style.use('dark_background')
@@ -95,66 +97,46 @@ def run_da_game(rows_cols, probability):
         # Delay between frames (adjust as needed)
         time.sleep(0.1)
 
+# end Functions
 
-
+# Text
 title = 'Jogo da vida de Conway'
+subsection = 'Quem foi Conway?'
 intro = 'Uma breve introdução sobre quem foi Conway\n' \
         'blabla\n' \
         'more bla bla'
+subsection_0 = 'Regras do jogo da vida'
 exp_0 = 'Uma breve explicação sobre as regras do jogo\n' \
                 'bla bla bla\n' \
-                'more bla bla bla bla bla'
-
-st.title(title)
-st.subheader('Quem foi Conway?')
-st.text(intro)
-st.subheader('Regras do jogo da vida')
-st.text(exp_0)
-
-st.subheader('Explicando o codigo')
-st.markdown('CRIANDO UMA "GRID"')
+                'more bla bla bla'
+subsection_1 ='Explicando o codigo'
+subsubsection_1 ='CRIANDO UMA "GRID"'
 exp_1 = 'Idealmente é necessario definirmos uma "grid" aonde definiremos quais pontos\n' \
                 'estão ativos, por simplicidade neste exercício vamos gerar aleatoriamente\n' \
-                'os pontos, de forma que celulas com valor de 1 serão consideradas ligadas\n' \
-                'e 0 desligadas. Observe o codigo abaixo:'
-st.text(exp_1)
-
-code_0 = '''
+                'os pontos, de forma que celulas com valor de 0 serão consideradas ligadas\n' \
+                'e -1 desligadas. Observe o codigo abaixo:'
+code_1 = '''
 import numpy as np 
 
 def create_grid(rows_cols, P):
     return np.random.choice([0, 1], size=(rows_cols, rows_cols), p=[1-P, P])
 '''
-st.code(code_0)
-exp_2 = 'Aqui geramos com a função "random.choice" do numpy, uma "grid" de pontos que são\n' \
+exp_1_2 = 'Aqui geramos com a função "random.choice" do numpy, uma "grid" de pontos que são\n' \
         '0 ou 1 e possuem dimenção rows_cols X rows_cols\n' \
         'e probabilidade P dos pontos estarem ligados.'
-st.text(exp_2)
-code_1 = '''
-grid = create_grid(5, 0.5)
+code_1_2 = '''
+grid = create_grid(5, 0.2)
 grid.show()
 '''
-st.code(code_1)
-with st.form('form0'):
-    button0 = False
-    grid = create_grid(5, 0.2)
-    button0 = st.form_submit_button('Roda!')
-    if button0 is True:
-        plot0(grid)
-        button0 =False
-
-st.markdown('ACHANDO VIZINHOS')
-
-exp_3 = 'O proximo passo agora seria aplicar as regras do jogo de Conway.\n' \
+subsubsection_1_2 = 'ACHANDO VIZINHOS'
+exp_1_3 = 'O proximo passo agora seria aplicar as regras do jogo de Conway.\n' \
         'Segundo o jogo se uma celula ativa esta cercada por 2 ou 3 outras\n' \
         'celulas vivas ela pode continuar vivendo, caso contrario morre,\n' \
         'se uma celula morta esta cercada por 3 vivas ela é ligada.\n' \
         'Logo o passo logico agora seria criar uma função que conta\n' \
         'as celulas vivas na vizinhança de um quadrado e aplica as\n' \
         'condições do jogo.'
-st.text(exp_3)
-
-code_2 = '''
+code_1_3 = '''
 def update_grid(grid):
     neighbours = np.zeros_like(grid)
     for i in range(-1, 2):
@@ -165,30 +147,76 @@ def update_grid(grid):
 
     new_grid = np.where((grid == 1) & ((neighbours < 2) | (neighbours > 3)), 0, grid)
     new_grid = np.where((grid == 0) & (neighbours == 3), 1, new_grid)
-    
+
     return new_grid
 '''
-st.code(code_2)
-
-exp_4 = 'Vamos olhar mais de perto o que eu considero a parte mais importante deste codigo,\n' \
+exp_1_4 = 'Vamos olhar mais de perto o que eu considero a parte mais importante deste codigo,\n' \
         'para contar os vizinhos de maneira rapida e eficiente resolvi pegar a matrix da grid\n' \
         'e mudar ela inteira em um movimento circular para que possamos somar os valores\n' \
         'laterais, a explicação visual a baixo ajudara a entender melhor. Segue o sequite\n' \
         'codigo que mostra visualmente o que esta acontecendo:'
-st.text(exp_4)
-code_3 = '''
+code_1_4 = '''
     for i in range(-1, 2):
         for j in range(-1, 2):
             if i == 0 and j == 0:
                 continue
             new_grid = np.roll(np.roll(grid, i, axis=0), j, axis=1)
 '''
-st.code(code_3)
+exp_1_5 = 'Agora que vimos como podemos somar valores\n' \
+        'vizinhos vamos estabelecer nossas regras.'
+code_1_5 = '''
+    new_grid = np.where((grid == 1) & ((neighbours < 2) | (neighbours > 3)), 0, grid)
+    new_grid = np.where((grid == 0) & (neighbours == 3), 1, new_grid)
+'''
+exp_1_6 = 'Nesta primeira linha estabelecemos se nossa celula esta ligada\n' \
+         ' e não tem 2 e nem 3 vizinhos proximos faça ela ser desligada\n' \
+         'e caso contrario apenas retorne o valor dela ( ela possui 2 ou 3 vizinhos),\n' \
+         'se ela esta deligada e tem 3 vizinhos ativos ela se torna ligada\n' \
+         'caso contrario apenas retorna seu valor. Agora vamos aplicar tudo\n' \
+         'e rodar o nosso jogo.'
+
+subsection_2 = 'O Jogo da vida de Conway!'
+
+link = 'Download para Windows: https://drive.google.com/file/d/1R5AscSyugvoo3TKQ5MjTdkF7-C0aqMnx/view?usp=sharing'
+# end text
+
+# Site code
+st.title(title)
+st.subheader(subsection)
+st.text(intro)
+st.subheader(subsection_0)
+st.text(exp_0)
+
+st.subheader(subsection_1)
+st.markdown(subsubsection_1)
+st.text(exp_1)
+st.code(code_1)
+st.text(exp_1_2)
+st.code(code_1_2)
+
+with st.form('form'):
+    button0 = False
+    grid = create_grid(5, 0.2)
+    button0 = st.form_submit_button('Roda!')
+    if button0 is True:
+        plot0(grid)
+        button0 =False
+
+st.markdown(subsubsection_1_2)
+st.text(exp_1_3)
+st.code(code_1_3)
+st.text(exp_1_4)
+st.code(code_1_4)
+
 with st.form('form1'):
     button = st.form_submit_button('Roda')
     reset = False
-    grid = np.zeros((5, 5))
+    grid = np.zeros((10, 10))
     grid[2, 2] = 1
+    grid[5, 6] = 1
+    grid[5, 4] = 1
+    grid[4, 5] = 1
+    grid[6, 5] = 1
 
     if button is True:
         ani1(grid)
@@ -200,23 +228,12 @@ with st.form('form1'):
     if reset is True:
         button = False
 
-exp_5 = 'Agora que vimos como podemos somar valores\n' \
-        'vizinhos vamos estabelecer nossas regras.'
-st.text(exp_5)
-code_4 = '''
-    new_grid = np.where((grid == 1) & ((neighbours < 2) | (neighbours > 3)), 0, grid)
-    new_grid = np.where((grid == 0) & (neighbours == 3), 1, new_grid)
-'''
-st.code(code_4)
-exp_6 = 'Nesta primeira linha estabelecemos se nossa celula esta ligada\n' \
-         ' e não tem 2 e nem 3 vizinhos proximos faça ela ser desligada\n' \
-         'e caso contrario apenas retorne o valor dela ( ela possui 2 ou 3 vizinhos),\n' \
-         'se ela esta deligada e tem 3 vizinhos ativos ela se torna ligada\n' \
-         'caso contrario apenas retorna seu valor. Agora vamos aplicar tudo\n' \
-         'e rodar o nosso jogo.'
-st.text(exp_6)
 
-st.subheader('O Jogo da vida de Conway!')
+st.text(exp_1_5)
+st.code(code_1_5)
+st.text(exp_1_6)
+
+st.subheader(subsection_2)
 
 with st.form('form2'):
     rows_cols = st.slider('Qual a dimenção da sua grid?', 0, 100)
@@ -225,5 +242,6 @@ with st.form('form2'):
     if submit_button and rows_cols != 0:
         run_da_game(rows_cols, probability)
 
-st.subheader('Link para download')
-st.markdown('https://drive.google.com/file/d/1R5AscSyugvoo3TKQ5MjTdkF7-C0aqMnx/view?usp=sharing')
+
+st.markdown(link)
+# End site code
